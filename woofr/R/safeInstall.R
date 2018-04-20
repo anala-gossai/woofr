dfc4f491df109304555b1a75c8f0cf1d3097cecc
analagossai <anala.gossai@gmail.com>
2018-04-20 11:26:54 -0400

    Updated the function input

--- woofr/R/safeInstall.R
+++ woofr/R/safeInstall.R
@@ -1,16 +1,16 @@
-safeInstall <- function(pckg) {
+safeInstall <- function(package_name) {
     # Summary: install a package from CRAN
     # without also updating the package dependencies 
     
     # Arg:
-    #   pckg: the names of the package in quotes 
+    #   package_name: the names of the package in quotes 
     
     # Return: the package is installed and loaded 
     require(devtools) # version ‘1.13.3’
     
-    install_cran(pckg, 
+    install_cran(package_name, 
                  upgrade_dependencies = FALSE)
     
-    library(pckg, 
+    library(package_name, 
             character.only = TRUE)
 }
